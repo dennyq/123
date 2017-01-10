e4fd46b85750b6435820903e5164302ec54c9dd8
eks <eklee.time@gmail.com>
2017-01-10 20:49:30 +0900

    =====

--- fix.sql
+++
@@ -1,4 +0,0 @@
-
-create table innercablesensorstate ( memberid varchar(10) not null,
-                                     temperature float(5,2) default -1 not null,
-                                     regtime char(14) not null);
